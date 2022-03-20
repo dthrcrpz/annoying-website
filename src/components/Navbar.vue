35c3d5ea850d501f92408d5b6f38da304be7d2ad
dthrcrpz <dthrcrpz@gmail.com>
2022-03-20 10:27:07 +0800

    setup tailwind, add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="shadow-md">
+    <div class="wrapper">
+      <div class="flex justify-center items-center">
+        <div>
+          <a href="javascript:void(0)" class="text-2xl font-bold">Annoying Site</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
