d0ab38aedd1872e34ed6f594361e719265b50245
dthrcrpz <dthrcrpz@gmail.com>
2022-03-20 19:56:27 +0800

    update nav title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <div class="wrapper">
       <div class="flex justify-center items-center">
         <div>
-          <a href="javascript:void(0)" class="text-2xl font-bold">Annoying Site</a>
+          <a href="javascript:void(0)" class="text-2xl font-bold">Annoying Website</a>
         </div>
       </div>
     </div>
